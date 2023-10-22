0d65c44c7b95f3fb71a2256eccc196b1751910d2
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-10-23 03:28:01 +0600

    Наконец получилось сделать правильную генерацию карт

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import './App.css';
 function Card() {
     const [hand, setHand] = useState([]); // Состояние для руки карт
 
+    const rank = {
+        1: 'A',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+        11: 'J',
+        12: 'Q',
+        13: 'K',
+    }
+
     const suits = {
         1: 'diams', // diams
         2: 'hearts', // hearts
@@ -11,11 +27,25 @@ function Card() {
         4: 'spades', // spades
     };
 
+    const suitsIc = {
+        1: '♦', // diams
+        2: '♥', // hearts
+        3: '♣', // clubs
+        4: '♠', // spades
+    };
+
     const generateNewHand = () => {
         const newHand = [];
+        const usedRanks = [];
         for (let i = 0; i < 4; i++) {
-            const newRank = Math.floor(Math.random() * 13) + 1; // Генерируем случайное число для ранга
-            const newSuit = Math.floor(Math.random() * 4) + 1; // Генерируем случайное число для масти
+            let newRank, newSuit;
+            do {
+                newRank = Math.floor(Math.random() * 13) + 1;
+            } while (usedRanks.includes(newRank));
+            usedRanks.push(newRank);
+            do {
+                newSuit = Math.floor(Math.random() * 4) + 1;
+            } while (newHand.some(card => card.rank === newRank && card.suit === newSuit));
             newHand.push({ rank: newRank, suit: newSuit });
         }
         setHand(newHand); // Обновляем состояние руки карт
@@ -24,10 +54,10 @@ function Card() {
     return (
         <div className="playingCards faceImages">
             {hand.map((card, index) => (
-                <span key={index} className={`card rank-${card.rank} ${suits[card.suit]}`}>
-          <span className="rank">{card.rank}</span>
-          <span className="suit">{suits[card.suit]}</span>
-        </span>
+                <span key={index} className={`card rank-${rank[card.rank].toLowerCase()} ${suits[card.suit]}`}>
+                    <span className="rank">{rank[card.rank]}</span>
+                    <span className="suit">{suitsIc[card.suit]}</span>
+                </span>
             ))}
 
             <button onClick={generateNewHand}>New hand</button>
